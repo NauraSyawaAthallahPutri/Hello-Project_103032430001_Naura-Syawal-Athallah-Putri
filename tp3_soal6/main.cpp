f9634eb42198a3d1f2ecd4250d6d59b3971aad8b
Naura <naurasap@gmail.com>
2025-10-29 16:46:42 +0700

    Menambahkan file tugas besar ke Hello Project

---
+++ tp3_soal6/main.cpp
@@ -0,0 +1,13 @@
+#include "Kerucut.h"
+
+int main() {
+    Kerucut k(7, 10); // jari-jari = 7, tinggi = 10
+
+    cout << "Kerucut dengan r = " << k.r << ", t = " << k.t << endl;
+    cout << "Garis pelukis    = " << k.garisPelukis() << endl;
+    cout << "Luas alas        = " << k.luasAlas() << endl;
+    cout << "Luas permukaan   = " << k.luasPermukaan() << endl;
+    cout << "Volume           = " << k.volume() << endl;
+
+    return 0;
+}
